e1a8de877bc1b48c7bee6f07fc9d95fa81c7169b
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2024-12-28 15:44:44 +0100

    Fix the build of mfem-sys with a system mfem
    Retrieve as much information as possible from cmake:
    - the name of the library,
    - the many include paths,
    - the C++ flags.
    Do not mandate a static lib — system libraries are in general dynamic.

--- crates/mfem-sys/build.rs
+++ crates/mfem-sys/build.rs
@@ -1,6 +1,4 @@
 /// The list of used MFEM libraries which needs to be linked with.
-const MFEM_LIBS: &[&str] = &["mfem"];
-
 fn main() {
     let target = std::env::var("TARGET").expect("No TARGET environment variable defined");
     let is_windows = target.to_lowercase().contains("windows");
@@ -12,9 +10,11 @@ fn main() {
         mfem_config.library_dir.to_str().unwrap()
     );
 
-    let lib_type = "static";
-    for lib in MFEM_LIBS {
-        println!("cargo:rustc-link-lib={lib_type}={lib}");
+    for lib in mfem_config.mfem_libs {
+        #[cfg(feature = "bundled")]
+        println!("cargo:rustc-link-lib=static={lib}");
+        #[cfg(not(feature = "bundled"))]
+        println!("cargo:rustc-link-lib={lib}");
     }
 
     if is_windows {
@@ -32,9 +32,13 @@ fn main() {
         .cpp(true)
         .std("c++14")
         .flag_if_supported("-w")
-        .include(mfem_config.include_dir)
-        .include("include")
-        .compile("wrapper");
+        .includes(mfem_config.include_dirs)
+        .include("include");
+    for f in mfem_config.cxx_flags {
+        build.flag_if_supported(f);
+    }
+
+    build.compile("wrapper");
 
     println!("cargo:rustc-link-lib=static=wrapper");
 
@@ -42,9 +46,12 @@ fn main() {
     println!("cargo:rerun-if-changed=include/wrapper.hpp");
 }
 
+#[derive(Debug)]
 struct MfemConfig {
-    include_dir: std::path::PathBuf,
+    mfem_libs: Vec<String>,
+    include_dirs: Vec<std::path::PathBuf>,
     library_dir: std::path::PathBuf,
+    cxx_flags: Vec<String>,
 }
 
 impl MfemConfig {
@@ -76,22 +83,38 @@ impl MfemConfig {
             .expect("Something went wrong when detecting MFEM.");
 
         let mut version: Option<semver::Version> = None;
-        let mut include_dir: Option<std::path::PathBuf> = None;
+        let mut mfem_libs: Vec<String> = vec![];
+        let mut include_dirs: Vec<std::path::PathBuf> = vec![];
         let mut library_dir: Option<std::path::PathBuf> = None;
+        let mut cxx_flags: Vec<String> = vec![];
 
         for line in cfg.lines() {
             if let Some((var, val)) = line.split_once('=') {
                 match var {
+                    // Keep in sync with "MFEM/CMakeLists.txt".
                     "VERSION" => version = semver::Version::parse(val).ok(),
-                    "INCLUDE_DIR" => include_dir = val.parse().ok(),
+                    "MFEM_LIBRARIES" => {
+                        for l in val.split(" ") { // FIXME: Right delim?
+                            mfem_libs.push(l.into());
+                        }
+                    }
+                    "INCLUDE_DIRS" => {
+                        for d in val.split(";") {
+                            include_dirs.push(d.into());
+                        }
+                    }
                     "LIBRARY_DIR" => library_dir = val.parse().ok(),
+                    "CXX_FLAGS" => {
+                        for f in val.split(" ") {
+                            cxx_flags.push(f.into());
+                        }
+                    }
                     _ => (),
                 }
             }
         }
 
-        if let (Some(version), Some(include_dir), Some(library_dir)) =
-            (version, include_dir, library_dir)
+        if let (Some(version), Some(library_dir)) = (version, library_dir)
         {
             if !version_req.matches(&version) {
                 #[cfg(feature = "bundled")]
@@ -100,12 +123,14 @@ impl MfemConfig {
 
                 #[cfg(not(feature = "bundled"))]
                 panic!("Pre-installed MFEM found but version is not met (found {} but {} required). Please provide required version or use `bundled` feature.",
-                       version, MFEM_VERSION);
+                       version, version_req);
             }
 
             Self {
-                include_dir,
+                mfem_libs,
+                include_dirs,
                 library_dir,
+                cxx_flags,
             }
         } else {
             panic!("MFEM found but something went wrong during configuration.");
